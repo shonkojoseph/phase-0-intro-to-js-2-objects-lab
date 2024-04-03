571f35964ad38cc7a9fa604003660c15440fbc8f
Joseph <josephshonko99@gmail.com>
2024-04-03 11:48:22 +0300

    commiting js object lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+const employee={
+    name:"sam",
+    streetAddress: '11 Broadway'
+
+}
+ 
+function updateEmployeeWithKeyAndValue(employee,key, value){
+    return{
+        ...employee,
+        [key]:value
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]= value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = { ...employee[key]}
+    delete newEmployee[key]
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee;
+}
